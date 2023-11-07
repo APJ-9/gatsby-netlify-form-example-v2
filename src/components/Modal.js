0ee6ba6b97ee3807db863f76cc9267b751e1a62d
Akhil P Jose <akhil.jose@vonnue.com>
2023-11-07 11:25:40 +0530

    form

---
+++ src/components/Modal.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../pages/index.css";
+
+const Modal = ({ code }) => {
+	return <div className="modal">Modal {code}</div>;
+};
+
+export default Modal;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,40 @@
 import * as React from "react";
 import "./index.css";
+import { navigate } from "gatsby";
+import Modal from "../components/Modal";
+import { useState } from "react";
+
 const IndexPage = () => {
+	const [formSubmit, setFormSubmit] = useState("");
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		const myForm = event.target;
+		const formData = new FormData(myForm);
+
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+				return response; // Pass the response to the next .then()
+			})
+			.then((response) => {
+				// Access the status code
+				const statusCode = response.status;
+				// Handle the response as needed
+				return response.text(); // Example: Return the response text
+			})
+
+			.catch((error) => {
+				alert(`Error: ${error.message}`);
+			});
+	};
+
 	return (
 		<main>
 			<h1>Netlify Form Integration</h1>
@@ -9,7 +43,7 @@ const IndexPage = () => {
 				netlify-honeypot="bot-field"
 				data-netlify="true"
 				name="contact"
-				action="/404"
+				onSubmit={handleSubmit}
 			>
 				<input type="hidden" name="bot-field" />
 				<input type="hidden" name="form-name" value="contact" />
